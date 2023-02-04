923804b539c29303b43f4eb01d8433540faeafe9
june.kim <junho85@gmail.com>
2023-02-04 11:48:53 +0900

    users.yaml 설정을 위한 github name, slack name 추출 테스트 코드 개선 test_slack_tools.test_get_users

--- attendance/slack_tools.py
+++ attendance/slack_tools.py
@@ -1,8 +1,10 @@
 import configparser
-from datetime import date, timedelta, datetime
-import slack
 import os
 import re
+from datetime import timedelta, datetime
+
+import slack
+
 
 class SlackTools:
     def __init__(self):
@@ -36,6 +38,52 @@ class SlackTools:
     def get_users(self):
         return self.slack_client.users_list()
 
+
+    '''
+    slack 유저 정보조회
+    @:param user: id (e.g. U04L6T16F0D)
+    @:return 
+    {'ok': True,
+     'user':
+      {'id': 'U04L6T16F0D',
+       'team_id': 'T04LZGEK6RE',
+       'name': 'junho85',
+       'deleted': False, 'color': '9f69e7',
+       'real_name': 'June Kim (김준호)',
+       'tz': 'Asia/Seoul', 'tz_label': 'Korea Standard Time', 'tz_offset': 32400,
+       'profile': {'title': '출석부관리자', 'phone': '', 'skype': '',
+                   'real_name': 'June Kim (김준호)',
+                   'real_name_normalized': 'June Kim (김준호)',
+                   'display_name': 'June Kim (김준호)',
+                   'display_name_normalized': 'June Kim (김준호)', 'fields': None, 'status_text': '', 'status_emoji': '',
+                   'status_emoji_display_info': [], 'status_expiration': 0, 'avatar_hash': '873b33c8a4d1',
+                   'image_original': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_original.jpg',
+                   'is_custom_image': True,
+                   'first_name': 'June', 'last_name': 'Kim (김준호)',
+                   'image_24': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_24.jpg',
+                   'image_32': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_32.jpg',
+                   'image_48': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_48.jpg',
+                   'image_72': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_72.jpg',
+                   'image_192': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_192.jpg',
+                   'image_512': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_512.jpg',
+                   'image_1024': 'https://avatars.slack-edge.com/2023-01-30/4703448917447_873b33c8a4d1b95cf440_1024.jpg',
+                   'status_text_canonical': '', 'team': 'T04LZGEK6RE'},
+       'is_admin': True,
+       'is_owner': True, 'is_primary_owner': True, 'is_restricted': False, 'is_ultra_restricted': False, 'is_bot': False,
+       'is_app_user': False, 'updated': 1675081703, 'is_email_confirmed': True,
+       'who_can_share_contact_card': 'EVERYONE'}
+    }
+    '''
+    def get_user(self, user):
+        return self.get_slack_client().users_info(
+            user=user
+        )
+
+    def get_users_id_name_dictionary(self):
+        users_list = self.slack_client.users_list()
+
+        return {member['id']: member['name'] for member in users_list["members"]}
+
     def get_user_names(self):
         return [user["name"] for user in self.get_users()["members"]]
 
@@ -55,6 +103,31 @@ class SlackTools:
         )
 
         return response
+
+    '''
+    자기소개채널에서 소개글을 보고 github name리스트 추출
+    @:param introduce_channel_id
+    @:return github name list
+    '''
+    def get_github_name_slack_id_dictionary_from_introduce_channel(self, introduce_channel_id):
+        p = re.compile('github.com/(\w+)')
+
+        github_name_slack_id_dict = {}
+
+        # 자기소개채널에서 github 주소가 있는 글 추출
+        response_conversations_history = self.get_slack_client().conversations_history(
+            channel=introduce_channel_id
+        )
+
+        for message in response_conversations_history["messages"]:
+            if "github.com" in message["text"]:
+                # github user name 추출
+                github_user_name = p.findall(message["text"])[0]
+                github_user_name = github_user_name.split("/")[0]
+
+                # slack user id 정보 조합
+                github_name_slack_id_dict[github_user_name] = message["user"]
+        return github_name_slack_id_dict
 
     def get_author_name_from_commit_message(self, message):
         p = re.compile("by (.*)")
--- attendance/test_slack_tools.py
+++ attendance/test_slack_tools.py
@@ -1,12 +1,16 @@
+from collections import defaultdict
 from unittest import TestCase
 
 from attendance.slack_tools import SlackTools
 from attendance.config_tools import ConfigTools
 import re
 
+
 class TestSlackTools(TestCase):
     slack_tools = SlackTools()
     config_tools = ConfigTools()
+
+    introduce_channel_id = "C04LA11FZ34"
 
     def test_get_users(self):
         print(self.slack_tools.get_users())
@@ -21,49 +25,74 @@ class TestSlackTools(TestCase):
     slack에 있는 유저들 - users.yaml에 등록된 slack이름을 뺌. 거기다가 봇들 제외
     다 등록 되었으면 결과에 아무 유저가 나오면 안됨
     '''
+
     def test_user_diff(self):
-        print(set(self.slack_tools.get_user_names())
-              - set(self.config_tools.get_user_slacknames())
-              - {"slackbot",
-                 "garden8",
-                 "github"})
+        users = set(self.slack_tools.get_user_names()) \
+                - set(self.config_tools.get_user_slacknames()) \
+                - {"slackbot",
+                   "garden8",
+                   "github"}
+
+        for user in users:
+            print(user)
 
     '''
     users.yaml에 등록된 slack이름 - slack에 있는 유저들
     '''
+
     def test_user_diff2(self):
         print(set(self.config_tools.get_user_slacknames())
               - set(self.slack_tools.get_user_names())
               )
 
+    def test_regex(self):
+        p = re.compile('https://github.com/(\w+)')
+        message = "<https://github.com/junho85>"
+
+        print(p.findall(message))
+
+    def test_slack_users(self):
+        # list to dictionary
+        slack_id_name_dict = self.slack_tools.get_users_id_name_dictionary()
+        # print(slack_id_name_dict)
+        for (id, name) in slack_id_name_dict.items():
+            print(id, name)  # e.g. U04L6T16F0D junho85
+
     '''
     자기소개 방에 있는 유저들의 github 주소를 기준으로 slack name 구하기
     '''
+
     def test_get_users(self):
-        p = re.compile('/github.com/(.*)/')
+        github_name_slack_id_dict = self.slack_tools.get_github_name_slack_id_dictionary_from_introduce_channel(
+            self.introduce_channel_id)
+        slack_id_name_dict = self.slack_tools.get_users_id_name_dictionary()
 
-        # 자기소개방에서 github 주소가 있는 글 추출
-        response = self.slack_tools.get_slack_client().conversations_history(
-            channel="C02MA9HUSV9"
-        )
+        github_name_slack_dict = {}
 
-        for message in response["messages"]:
-            if "github.com" in message["text"]:
-                github_id = p.findall(message["text"])[0]
-                github_id = github_id.split("/")[0]
-                # print(message["user"])
-                response2 = self.slack_tools.get_slack_client().users_info(
-                    user=message["user"]
-                )
-                print(github_id + ":")
-                print("  slack: " + response2["user"]["name"])
-                # print(message["text"])
+        for github_name, slack_id in github_name_slack_id_dict.items():
+            github_name_slack_dict[github_name] = {"slack_name": slack_id_name_dict[slack_id]}
+        # print("-----")
+        # print(github_name_slack_id_dict)
+        # print("-----")
+        # print(slack_id_name_dict)
+
+        print(github_name_slack_dict)
+
+        for github_name, slack in github_name_slack_dict.items():
+            print(f'''{github_name}:
+  slack: {slack["slack_name"]}''')
+
+    def test_user(self):
+        user_id = "U04L6T16F0D"
+        r = self.slack_tools.get_user(user_id)
+        print(r)
 
     '''
     channel list 구하기.
      - 자기소개 channel id: C02MA9HUSV9
      - commit channel id: C02N0D83A3A
     '''
+
     def test_get_channels(self):
         result = self.slack_tools.get_slack_client().conversations_list()
         print(result)
@@ -86,5 +115,6 @@ class TestSlackTools(TestCase):
         print(response)
 
     def test_get_author_name_from_commit_message(self):
-        github_id = self.slack_tools.get_author_name_from_commit_message("[junho85/TIL] 2 new commits pushed  to _master_ by junho85")
+        github_id = self.slack_tools.get_author_name_from_commit_message(
+            "[junho85/TIL] 2 new commits pushed  to _master_ by junho85")
         print(github_id)
